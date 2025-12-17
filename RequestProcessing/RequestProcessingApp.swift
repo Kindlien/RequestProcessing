d7e51ca3742a0e80e29d6b474969fcc9e8d503c5
William Kindlien <william.k.gunawan@gmail.com>
2025-12-17 10:46:03 +0700

    Initial Commit

---
+++ RequestProcessing/RequestProcessingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RequestProcessingApp.swift
+//  RequestProcessing
+//
+//  Created by William Kindlien Gunawan on 17/12/25.
+//
+
+import SwiftUI
+
+@main
+struct RequestProcessingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
